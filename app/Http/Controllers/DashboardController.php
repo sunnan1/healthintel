7a8c1a6b5b904e2b93f192ea736dd5e929454e21
salman <salmanatta123@gmail.com>
2024-01-24 13:24:34 +0500

    Changes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function showDashboard() {
-        return view("admin.buttons");
+        return view("admin.employee-registration");
     }
 }
